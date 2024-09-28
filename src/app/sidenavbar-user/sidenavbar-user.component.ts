907c73f728f1f6a2463ea500165c9d35dccd5494
Chandu Maharana <chandumaharana58537@gmail.com>
2024-09-28 13:02:33 +0530

    the side nvbar issue was resolved

--- src/app/sidenavbar-user/sidenavbar-user.component.ts
+++ src/app/sidenavbar-user/sidenavbar-user.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HardcodedAuthenticationService } from '../authentication.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHome ,faUser,faTractor,faSeedling,faCog,faSignOutAlt} from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-sidenavbar-user',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,FontAwesomeModule],
   templateUrl: './sidenavbar-user.component.html',
   styleUrl: './sidenavbar-user.component.css'
 })
 export class SidenavbarUserComponent {
+  faHome = faHome;
+  faUser = faUser;
+  faTractor = faTractor;
+  faSeedling = faSeedling;
+  faCog = faCog;
+  faSignOutAlt = faSignOutAlt;
   constructor(public hardcodedAuthenticationService: HardcodedAuthenticationService) {}
 }
